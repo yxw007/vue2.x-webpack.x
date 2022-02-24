6c2e9986a408b326931b5bb00a3dfce7c96992dc
Potter <75838259@qq.com>
2022-02-24 15:12:46 +0800

    fix: smp包裹一层会导致HtmlWebpackPlugin.getHook重新创建hooks对象，导致auto-external-plugin注册的alterAssetTags事件丢失，以至于无法注入配置的CDN，所以将smp挪至仅用于npm run build:analyzer方式打包查看分析结果

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -35,96 +35,105 @@ threadLoader.warmup(
   ["cache-loader", "style-loader", "css-loader", "postcss-loader", "less-loader", "sass-loader", "sass-resources-loader"]
 ); */
 
-const wrapConfig = smp.wrap(
-  merge(baseWebpackConfig, {
-    mode,
-    output: {
-      path: config.build.assetsRoot,
-      filename: utils.assetsPath("js/[name].[chunkhash].js"),
-      chunkFilename: utils.assetsPath("js/[id].[chunkhash].js"),
-      publicPath: config.build.assetsPublicPath,
-    },
-    devtool: config.build.devtool,
-    optimization: {
-      minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
-      usedExports: true,
-    },
-    // sideEffects: ["*.css"],
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
-        },
-        {
-          test: /\.less$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "less-loader"],
-        },
-        {
-          test: /\.s[ac]ss$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            "css-loader",
-            "postcss-loader",
-            // "thread-loader",
-            "sass-loader",
-            {
-              loader: "sass-resources-loader",
-              options: {
-                resources: [utils.resolve("src/style/variables.scss")],
-              },
+let webpackConfig = merge(baseWebpackConfig, {
+  mode,
+  output: {
+    path: config.build.assetsRoot,
+    filename: utils.assetsPath("js/[name].[chunkhash].js"),
+    chunkFilename: utils.assetsPath("js/[id].[chunkhash].js"),
+    publicPath: config.build.assetsPublicPath,
+  },
+  devtool: config.build.devtool,
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    usedExports: true,
+  },
+  // sideEffects: ["*.css"],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "less-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          // "thread-loader",
+          "sass-loader",
+          {
+            loader: "sass-resources-loader",
+            options: {
+              resources: [utils.resolve("src/style/variables.scss")],
             },
-          ],
-        },
-        {
-          test: /\.styl$/,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "stylus-loader"],
-        },
-      ],
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        "process.env": env,
-      }),
-      new CleanWebpackPlugin({
-        output: {
-          path: config.build.assetsRoot,
-        },
-      }),
-      // new MiniCssExtractPlugin({
-      //   filename: utils.assetsPath("css/[name].[hash].css"),
-      //   ignoreOrder: true,
-      // }),
-      new HtmlWebpackPlugin({
-        filename: "index.html",
-        template: utils.resolve(`public/index.html`),
-      }),
-      new AutoExternalPlugin(config.build.externalLibs),
+          },
+        ],
+      },
+      {
+        test: /\.styl$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "stylus-loader"],
+      },
     ],
-    performance: {
-      hints: false,
-      maxEntrypointSize: 512000,
-      maxAssetSize: 512000,
-    },
-    externals: {
-      exceljs: "ExcelJS",
-    },
-  })
-);
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": env,
+    }),
+    new CleanWebpackPlugin({
+      output: {
+        path: config.build.assetsRoot,
+      },
+    }),
+    // new MiniCssExtractPlugin({
+    //   filename: utils.assetsPath("css/[name].[hash].css"),
+    //   ignoreOrder: true,
+    // }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: utils.resolve(`public/index.html`),
+    }),
+    new AutoExternalPlugin(config.build.externalLibs),
+  ],
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  externals: {
+    exceljs: "ExcelJS",
+  },
+});
 
-//说明：由于VueLoaderPlugin和MiniCssExtractPlugin与smp版本不兼容，报："You forgot to add 'mini-css-extract-plugin' plugin"，所以需要单独抽出来此处
-//参考文献：https://github.com/stephencookdev/speed-measure-webpack-plugin/issues/167
-wrapConfig.plugins.push(
+/* 后置插件 */
+const tailPlugins = [
   new VueLoaderPlugin(),
+
   new SpriteLoaderPlugin({ plainSprite: false }),
   new MiniCssExtractPlugin({
     filename: utils.assetsPath("css/[name].[contenthash].css"),
     ignoreOrder: true,
-  })
-);
+  }),
+];
 
 if (isShowAnalyzer) {
+  //说明：smp包裹一层会导致HtmlWebpackPlugin.getHook重新创建hooks对象，导致auto-external-plugin注册的alterAssetTags事件丢失，以至于无法注入配置的CDN
+  //所以将smp挪至仅用于npm run build:analyzer方式打包查看分析结果
+  const wrapConfig = smp.wrap(webpackConfig);
+
+  //说明：由于VueLoaderPlugin和MiniCssExtractPlugin与smp版本不兼容，报："You forgot to add 'mini-css-extract-plugin' plugin"，所以需要单独抽出来此处
+  //参考文献：https://github.com/stephencookdev/speed-measure-webpack-plugin/issues/167
+  wrapConfig.plugins.push(tailPlugins);
   wrapConfig.plugins.push(new BundleAnalyzerPlugin());
+
+  webpackConfig = wrapConfig;
+} else {
+  webpackConfig.plugins.push(...tailPlugins);
 }
 
-module.exports = wrapConfig;
+module.exports = webpackConfig;
