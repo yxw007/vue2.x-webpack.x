2c54e57a30518b6731264c1dca56aeb366870ba3
Potter <75838259@qq.com>
2022-02-25 21:39:03 +0800

    调整：由于vue\vuex\vue-router cdn integrity 签名被block 导致无法引入，没找到解决办法，暂时先关闭integrity

--- config/index.js
+++ config/index.js
@@ -7,18 +7,26 @@ function getExternalLibs(isMin) {
     vue: {
       globalVariable: "Vue",
       url: `https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue${extension}`,
+      // integrity: "sha256-kXTEJcRFN330VirZFl6gj9+UM6gIKW195fYZeR3xDhc=",
+      async: false,
     },
     "vue-router": {
       globalVariable: "VueRouter",
       url: `https://cdn.jsdelivr.net/npm/vue-router@3.0.1/dist/vue-router${extension}`,
+      // integrity: "sha256-yEB9jUlD51i5kxJZlzgzfR6XmVKI76Nl1WRA1aqIilU=",
+      async: false,
     },
     vuex: {
       globalVariable: "Vuex",
       url: `https://cdn.jsdelivr.net/npm/vuex@3.0.1/dist/vuex${extension}`,
+      // integrity: "sha256-1QlN0ckC4jlz91DZixPZxTv9vYpcBmS7sK7HA8xFmFA=",
+      async: false,
     },
     "element-ui": {
       globalVariable: "Element",
       url: `https://unpkg.com/element-ui/lib/index.js`,
+      position: "body",
+      async: false,
     },
     "element-ui/lib/theme-chalk/index.css": {
       isCSSModule: true,
