96ca94acf43e6b05e5a8d8fbc5a4c057a81c53c6
Potter <75838259@qq.com>
2022-03-04 22:33:01 +0800

    fix: delete unuse code

--- src/main.js
+++ src/main.js
@@ -4,17 +4,6 @@ import router from "./router";
 import store from "./store";
 import "element-ui";
 
-function abc() {
-  return "this is abc";
-}
-
-function testFun() {
-  var name = "potter";
-  let age = 1;
-  // console.log(name, age, abc());
-}
-testFun();
-
 export default new Vue({
   el: "#app",
   router,
